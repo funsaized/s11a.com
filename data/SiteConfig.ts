029f3095c60733adb0d71cf9350ceb70048fc156
funsaized <saiguy@icloud.com>
2025-08-14 20:42:48 -0400

    chore: update

--- data/SiteConfig.ts
+++ data/SiteConfig.ts
@@ -18,8 +18,7 @@ const config: Config = {
   userTwitter: "Funsaized", // Optionally renders "Follow Me" in the UserInfo segment.
   userLocation: "NC, USA", // User location to display in the author segment.
   userAvatar: "https://api.adorable.io/avatars/150/test.png", // User avatar to display in the author segment.
-  userDescription:
-    "Yeah, I like animals better than people sometimes... Especially dogs. Dogs are the best. Every time you come home, they act like they haven't seen you in a year. And the good thing about dogs... is they got different dogs for different people.", // User description to display in the author segment.
+  userDescription: "building in public 🚀 | there’s no place like 127.0.0.1", // User description to display in the author segment.
   // Links to social profiles/projects you want to display in the author segment/navigation bar.
   userLinks: [
     {
