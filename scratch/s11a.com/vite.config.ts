d65c421c9dac2f07a4474558a7dc30461c3f261d
funsaized <saiguy@icloud.com>
2026-08-16 13:47:03 -0400

    chore: migrate to oxfmt and oxlint

--- scratch/s11a.com/vite.config.ts
+++ scratch/s11a.com/vite.config.ts
@@ -1,19 +1,16 @@
-import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
-
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import netlify from '@netlify/vite-plugin-tanstack-start'
+import netlify from "@netlify/vite-plugin-tanstack-start";
+import tailwindcss from "@tailwindcss/vite";
+import { devtools } from "@tanstack/devtools-vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 const config = defineConfig({
-  server: {
-    allowedHosts: ['blog-new.dev.s11a.com'],
-  },
-  resolve: { tsconfigPaths: true },
-  plugins: [devtools(), netlify(), tailwindcss(),
-   tanstackStart(), viteReact()],
-})
+	server: {
+		allowedHosts: ["blog-new.dev.s11a.com"],
+	},
+	resolve: { tsconfigPaths: true },
+	plugins: [devtools(), netlify(), tailwindcss(), tanstackStart(), viteReact()],
+});
 
-export default config
+export default config;
